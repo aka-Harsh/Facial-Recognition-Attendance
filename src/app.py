fc1cd9a080a5584da204f50f8960cef68b96a2fa
Harsh <harsh.work8421@gmail.com>
2024-09-06 17:58:16 +0530

    file structure updated and attendance.csv added

--- src/app.py
+++ src/app.py
@@ -4,4 +4,4 @@ import pandas as pd
 import os
 from datetime import date
 from face_recognition import load_known_faces, recognize_faces
-from attendance import load_daily_attendance, update_attendance, save_daily_attendance, get_attendance_summary
+from src.attendance import load_daily_attendance, update_attendance, save_daily_attendance, get_attendance_summary
