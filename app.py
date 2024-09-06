16e4bbd7b624fcac5dbdf77139a09b0cf14ec932
Harsh <harsh.work8421@gmail.com>
2024-09-06 17:55:21 +0530

    app.py Updated and attendance.py created

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import cv2
+import pandas as pd
+import os
+from datetime import date
+from face_recognition import load_known_faces, recognize_faces
+from attendance import load_daily_attendance, update_attendance, save_daily_attendance, get_attendance_summary
