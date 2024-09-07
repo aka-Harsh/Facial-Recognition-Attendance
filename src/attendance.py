f364ce42a5e877df8dd299e3cd159c3d1c8f0e9e
Harsh <harsh.work8421@gmail.com>
2024-09-07 13:09:27 +0530

    attendance.py Updated

--- src/attendance.py
+++ src/attendance.py
@@ -1,3 +1,16 @@
 import pandas as pd
 from datetime import datetime
-import os
+
+def load_attendance(csv_path):
+    return pd.read_csv(csv_path)
+
+def update_attendance(df, recognized_names):
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    for name in recognized_names:
+        if name != "Unknown":
+            df.loc[df['Student Name'] == name, 'Status'] = 'Present'
+            df.loc[df['Student Name'] == name, 'Last Seen'] = current_time
+    return df
+
+def save_attendance(df, csv_path):
+    df.to_csv(csv_path, index=False)
